bf2d72f604d6f048c238fb2055a0a6c119be2dab
adrasa.cantya <adrasa.cantya@ui.ac.id>
2024-10-07 11:07:54 +0700

    feat: initiating Spring Project Appointment

---
+++ src/main/java/apap/ti/appointment2206829603/Appointment2206829603Application.java
@@ -0,0 +1,13 @@
+package apap.ti.appointment2206829603;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appointment2206829603Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appointment2206829603Application.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/appointment2206829603/Appointment2206829603ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.appointment2206829603;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Appointment2206829603ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
